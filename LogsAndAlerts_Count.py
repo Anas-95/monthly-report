670bab66305f012b4cac7ff389a2ba5ecf798b2a
Anas <anas95y@gmail.com>
2020-11-09 08:11:47 +0300

    Init

--- LogsAndAlerts_Count.py
+++ LogsAndAlerts_Count.py
@@ -5,33 +5,34 @@ import pandas as pandas
 from os import system, path
 import urllib3
 from json.decoder import JSONDecodeError
-from datetime import timedelta
-from datetime import datetime
-from config import Config
-from helpers import CustomTime, CustomFiles
+from datetime import datetime, timedelta
+from config import get_headers, get_dir, get_logs_dir
+from helpers import hour_of_day, to_ms_timestamp, read_json
+from os import system, path
 
 
 if __name__ == "__main__":
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     session = requests.session()
-    headers = Config.get_headers()
-    # logs_count = CustomFiles.read_json("/home/scripts/offense-stat/exports/arc/ec_2020-44.json")['Count']
-    # logs_count = f"{int(sum(logs_count.values())):,}"
+    headers = get_headers()
     today = datetime.today()
-    s_time = hour_of_day(today_st, 1)
-    e_time = hour_of_day(today_st, 1)
-    timestamp_s_time = CustomTime.to_ms_timestamp(s_time)
-    timestamp_e_time = CustomTime.to_ms_timestamp(e_time)
-    print(today.strftime("%Y-%U"))
+    current_year = today.strftime("%Y")
+    current_week = today.strftime("%U")
+    weeks = [eval(current_week + '-i') for i in range(4)]
+    logs_count = [read_json(f"/home/scripts/offense-stat/exports/arc/ec_{current_year}-{week}.json")['Count'] for week in weeks]
+    logs_count = f"{int(sum(logs_count.values())):,}"
+    # s_time = hour_of_day(today_st, 1)
+    # e_time = hour_of_day(today_st, 1)
+    # timestamp_s_time = to_ms_timestamp(s_time)
+    # timestamp_e_time = to_ms_timestamp(e_time)
+    print(current_week)
     exit(0)
-    resp = session.get(f'https://ry1-core-siem.sic.sitco.sa/api/siem/offenses?fields=domain_id&filter=start_time>={timestamp_e_time} AND start_time<={timestamp_e_time}', headers=headers, verify=False)
+    resp = session.get(f'https://ry1-core-siem.sic.sitco.sa/api/siem/offenses?fields=domain_id&filter=start_time>={timestamp_s_time} AND start_time<={timestamp_e_time}', headers=headers, verify=False)
     if resp.status_code == 200:
         resp = resp.json()
-        resp2 = resp2.json()
-        if len(resp) > 0 and len(resp2) > 0:
-            write_offenses_to_file(session, resp, resp2, tenants)
-            offenses_path = os.path.abspath(os.path.join(get_dir(),"Offenses_Count.csv"))
-            os.system(f'echo "File attached" | mail -s "Offenses Count" -a {offenses_path} -c "aalmohsen@site.sa, ralharbi@site.sa" SOCL2@site.sa')
+        if len(resp) > 0:
+            write_offenses_to_file(session, resp, tenants)
+            offenses_path = path.abspath(path.join(get_dir(), "Offenses_Count.csv"))
     else:
         print("Error: ", resp.status_code)
--- config.py
+++ config.py
@@ -1,25 +1,20 @@
-# import os
+# Returns API headers of QRadar
+def get_headers():
+    return {
+        'Version': '11.0',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'SEC': '1fdb30bf-ac3c-4563-95e9-f8e326095604'
+    }
 
 
-class Config:
-    def __init__(self):
-        pass
-    
-    # Returns API headers of QRadar
-    def get_headers():
-        return {
-            'Version': '11.0',
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'SEC': '1fdb30bf-ac3c-4563-95e9-f8e326095604'
-        }
-    
-    # Returns current directory
-    def get_dir():
-        return os.path.dirname(__file__)
-    
-    # Returns logs directory for current script
-    def get_logs_dir():
-        return os.path.join(os.path.dirname(__file__), "logs")
+# Returns current directory
+def get_dir():
+    return os.path.dirname(__file__)
+
+
+# Returns logs directory for current script
+def get_logs_dir():
+    return os.path.join(os.path.dirname(__file__), "logs")
 
 
